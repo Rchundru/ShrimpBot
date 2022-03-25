2865a863335bcb8aa89f647318df19746cfe182e
rchundru <rohitchundru@gmail.com>
2022-03-25 11:39:03 -0500

    new functionality

--- index.js
+++ index.js
@@ -1,18 +1,67 @@
 //run using command in terminal: node index.js
 const Discord = require("discord.js")
+const dotenv = require("dotenv")
+const { REST } = require("@discordjs/rest")
+const { Routes } = require("discord-api-types/v9")
+const { Player } = require("discord-player")
 const TOKEN = "secret"
 const client = new Discord.Client({
     intents: [
         "GUILDS",
-        "GUILD_MESSAGES"
+        "GUILD_MESSAGES",
+        "GUILD_VOICE_STATES"
     ]
 })
+client.slashcommands = new Discord.Collection();
+client.player = new Player(client, {
+    ytdlOptions:{
+        quality:"highestaudio",
+        highWaterMark: 1 << 25
+    }
+})
+const LOAD_SLASH = process.argv[2] == 'load'
+const CLIENT_ID = "956578747841134593"
+const GUILD_ID = "513466762004791300"
 //var count
 var fs = require("fs");
-
+const { loadavg } = require("os")
+let commands = []
+const slashFiles = fs.readdirSync("./slash").filter(file => file.endsWith(".js"))
+for (const file of slashFiles){
+    const slashcmd = require(`./slash/${file}`)
+    client.slashcommands.set(slashcmd.data.name, slashcmd)
+    if (LOAD_SLASH) commands.push(slashcmd.data.toJSON())
+}
+if (LOAD_SLASH) {
+    const rest = new REST({ version: "9" }).setToken(TOKEN)
+    console.log("Deploying slash commands")
+    rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), {body: commands})
+    .then(() => {
+        console.log("Successfully loaded")
+        process.exit(0)
+    })
+    .catch((err) => {
+        if (err){
+            console.log(err)
+            process.exit(1)
+        }
+    })
+}else{
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}`)
     client.user.setActivity("Made because of lah700");
+})
+client.on("interactionCreate", (interaction) => {
+    async function handleCommand() {
+        if (!interaction.isCommand()) return
+
+        const slashcmd = client.slashcommands.get(interaction.commandName)
+        if (!slashcmd) interaction.reply("Not a valid slash command")
+
+        await interaction.deferReply()
+        await slashcmd.run({ client, interaction })
+    }
+    handleCommand()
 })
 
 client.on("messageCreate", (message) => {
@@ -100,6 +149,9 @@ client.on("messageCreate", (message) => {
     }
     if(text.includes("$about")){
         message.reply("A bot created by Rohit & Mitch because Salah is obsessed with shrimp. Will keep track of the number of times shrimp has been said")
+        message.reply("Flip a coin using $coin")
+        message.reply("Learn about shrimp dishes using $dishes")
+        message.reply("Play music using /play song or /play search")
     }
     if(text.includes("$coin")){
         var value = Math.floor(Math.random() * 2);
@@ -120,3 +172,4 @@ client.on("messageCreate", (message) => {
     }
 })
 client.login(TOKEN)
+}
