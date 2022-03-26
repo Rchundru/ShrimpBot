4250fab27be6ae32d6bcd03a4c63e000c3dcf3d0
rchundru <rohitchundru@gmail.com>
2022-03-26 17:24:57 -0500

    Cleaned up code

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const dotenv = require("dotenv")
 const { REST } = require("@discordjs/rest")
 const { Routes } = require("discord-api-types/v9")
 const { Player } = require("discord-player")
-const TOKEN = "secret"
+const TOKEN = "OTU2NTc4NzQ3ODQxMTM0NTkz.YjyRYQ.Go3BQCEAhr0zqtNp7DWw7stl-pU"
 const client = new Discord.Client({
     intents: [
         "GUILDS",
@@ -22,7 +22,6 @@ client.player = new Player(client, {
 const LOAD_SLASH = process.argv[2] == 'load'
 const CLIENT_ID = "956578747841134593"
 const GUILD_ID = "513466762004791300"
-//var count
 var fs = require("fs");
 const { loadavg } = require("os")
 let commands = []
@@ -65,6 +64,17 @@ client.on("interactionCreate", (interaction) => {
 })
 
 client.on("messageCreate", (message) => {
+
+    function incCount(){
+        let count = parseInt(fs.readFileSync('shrimpCount.txt', "utf8"));
+        count++;
+        fs.writeFile('shrimpCount.txt', count.toString(), function(err) {
+            if (err) {
+               return console.error(err);
+            }
+        });
+        return count;
+    }
     var text = message.content.toLocaleLowerCase();
     if (message.author.bot) return;
     if(text.includes("smh") && message.author.id==249753941535883264){
@@ -79,96 +89,43 @@ client.on("messageCreate", (message) => {
                    return console.error(err);
                 }
             });
-         
-
-            message.channel.send({content: "I smh to your smh Salah, you have said smh "+count+" times now.", files: [ "./200w.gif"]})
-        
+            message.channel.send({content: "I smh to your smh Salah, you have said smh "+count+" times now.", files: [ "./200w.gif"]})     
     });
-
     }
     if(text.includes("shrimp")){
-        //count++;
-        fs.readFile('shrimpCount.txt', function (err, data) {
-            if (err) {
-               return console.error(err);
-            }
-             var count = parseInt(data.toString());
-             count++;
-             fs.writeFile('shrimpCount.txt', count.toString(), function(err) {
-                if (err) {
-                   return console.error(err);
-                }
-            });
+        var count = incCount();
          
         if(message.author.id==249753941535883264){
             message.channel.send({content: "SALAH STOP MENTIONING THAT CRUSTACEAN! It has been said "+count+" times now!", files: [ "./scrikmp.jpg"]})
         }else{ 
             message.channel.send({content: "Which one of you FLAT FOOTED CHILDREN mentioned that crustacean?! It has been said "+count+" times now!", files: [ "./scrikmp.jpg"]})
         }
-    });
     }
     if(text.includes("prawn")){
-        //count++;
-        fs.readFile('shrimpCount.txt', function (err, data) {
-            if (err) {
-               return console.error(err);
-            }
-             var count = parseInt(data.toString());
-             count++;
-             fs.writeFile('shrimpCount.txt', count.toString(), function(err) {
-                if (err) {
-                   return console.error(err);
-                }
-            });
+        var count = incCount();
         if(message.author.id==249753941535883264){
             message.channel.send({content: "Nice try Salah, it's the same thing! It has been said "+count+" times now!", files: [ "./scrikmp.jpg"]})
         }else{ 
             message.channel.send({content: "WHHOOOO MENTIONED THAT CRUSTACEAN?! It has been said "+count+" times now!", files: [ "./scrikmp.jpg"]})
         }
-    });
     }
     if(text.includes("caridea")){
-        //count++;
-        fs.readFile('shrimpCount.txt', function (err, data) {
-            if (err) {
-               return console.error(err);
-            }
-             var count = parseInt(data.toString());
-             count++;
-             fs.writeFile('shrimpCount.txt', count.toString(), function(err) {
-                if (err) {
-                   return console.error(err);
-                }
-            });
+        var count = incCount();
         if(message.author.id==249753941535883264){
             message.channel.send({content: "Getting all sciency are we Salah? It won't work! You have mentioned that crustacean "+count+" times now!", files: [ "./scrikmp.jpg"]})
         }else{ 
             message.channel.send({content: "Nice try, I know what that means! It has been mentioned "+count+" times now!", files: [ "./scrikmp.jpg"]})
         }
-    });
     }
     if(text.includes("camarón") || text.includes("camaron")){
-        //count++;
-        fs.readFile('shrimpCount.txt', function (err, data) {
-            if (err) {
-               return console.error(err);
-            }
-             var count = parseInt(data.toString());
-             count++;
-             fs.writeFile('shrimpCount.txt', count.toString(), function(err) {
-                if (err) {
-                   return console.error(err);
-                }
-            });
+            var count = incCount();
         if(message.author.id==249753941535883264){
             message.channel.send({content: "¡Hablar en español no me engañará, Salah! ¡Ya lo has dicho "+count+" veces!", files: [ "./spanishGru.jpg"]})
         }else{ 
             message.channel.send({content: "¡Hablar español no funcionará! Se ha mencionado "+count+" tomos ahora.", files: [ "./spanishGru.jpg"]})
         }
-    });
     }
     if((text.includes("bitch") || text.includes("hoe")) && message.author.id==513466168154128415){
-        //count++;
         message.reply("No u");
     }
     if(text.includes("$about")){
